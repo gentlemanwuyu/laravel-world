0b2aca76a5855f90fb76b05edb532e449965b3b3
wuyu <492444775@qq.com>
2019-10-11 23:54:21 +0800

    chineseTree Method

--- src/World.php
+++ src/World.php
@@ -2,6 +2,8 @@
 
 namespace Wuyu\World;
 
+use Illuminate\Support\Facades\DB;
+
 class World
 {
     /**
@@ -12,8 +14,25 @@ class World
 
     }
 
+    /**
+     * Recursively get sub regions
+     *
+     * @param int $parent_id
+     * @return array
+     */
     public function chineseTree($parent_id = 0)
     {
+        $regions = DB::table('chinese_regions')->where('parent_id', $parent_id)->get();
+        $regions = array_column($regions, null, 'id');
+        foreach ($regions as $id => $region) {
+            $region = (array)$region;
+            $children = $this->chineseTree($region['id']);
+            if ($children) {
+                $region['children'] = $children;
+            }
+            $regions[$id] = $region;
+        }
 
+        return $regions;
     }
 }
